bdbc9155d7300c7d0b35c4a980edbb72443d1c19
Gurmeet Sandhu <gurmeet990@gmail.com>
2022-02-11 09:52:43 -0800

    first commit

---
+++ main/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.91.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "resourcegroup-test"
+  location = "westus2"
+}
