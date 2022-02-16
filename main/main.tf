7d0939fe627dcf453004906afefad6dd7657970d
Gurmeet Singh Sandhu <30559835+Gurmeet-Sandhu@users.noreply.github.com>
2022-02-15 22:07:13 -0800

    Update main.tf

--- main/main.tf
+++ main/main.tf
@@ -23,8 +23,3 @@ resource "azurerm_resource_group" "rg" {
   name     = "resourcegroup-test"
   location = "westus2"
 }
-
-resource "azurerm_resource_group" "another-rg" {
-  name     = "another-resourcegroup-test"
-  location = "westus2"
-}
